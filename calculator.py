03a595a59cfa9d4aa5b1c1e7237331e307aa59d0
Weshalldoit <106491586+Weshalldoit@users.noreply.github.com>
2022-05-30 01:25:50 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,30 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    
+    if type(y) != 0\
+    return "Invalid value for denominator, cant't divide by 0!"
+
+   return x / y
 
 
 def multiply(x, y):
-    pass
+    return x*y
 
 
 def square(x):
-    pass
+    return x**2
 
 
 def power(x, y):
-    pass
+    
+    power = pow(x,y)
+    return power
 
 
 def sqrt(x):
-    pass
+    return sqrt x
